1a45415e1bbe1e745b7e162447c6867775af7548
Sebastian <Handyfreak011@gmail.com>
2017-01-26 17:02:06 +0100

    cleaning out

--- product-voter.php
+++ product-voter.php
@@ -7,23 +7,3 @@
 * Version: 0.0.1
 * License: GLPv2
 */
-
-function iddt_remove_dashboard_widget() {
-  remove_meta_box('dashboard_primary','dashboard','postbox');
-}
-
-add_action('admin_init','iddt_remove_dashboard_widget');
-
-
-function iddt_add_google_analytics(){
-
-  global $wp_admin_bar;
-  $wp_admin_bar->add_menu( array(
-    'id' => 'google_analytics',
-    'title' => 'Google Analytics',
-    'href' => 'https://www.google.de/intl/de/analytics/'
-  ));
-
-}
-add_action('wp_before_admin_bar_render','iddt_add_google_analytics');
- ?>
