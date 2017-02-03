464f334bbc5feb615bb014b29f9fa1d69813d88f
Sebastian <Handyfreak011@gmail.com>
2017-02-03 15:07:40 +0100

    Adding folders and files for css and js, also properly enqueuing these

--- product-voter.php
+++ product-voter.php
@@ -17,4 +17,19 @@ require_once(plugin_dir_path(__FILE__)) .  'product-voter-cpt.php';
 require_once(plugin_dir_path(__FILE__)) .  'product-voter-render-admin.php';
 require_once(plugin_dir_path(__FILE__)) .  'project-voter-fields.php';
 
+
+function iddt_voter_admin_enqueue_scripts(){
+  //Variables to target the post type and post screen
+  global $pagenow, $typenow;
+  var_dump($typenow);
+
+  if (($pagenow == 'post.php' || $pagenow == 'post-new.php') &&  $typenow == 'produktart'){
+    //Main plugin css file
+    wp_enqueue_style('iddt-admin.css', plugins_url('css/admin-voter.css', __FILE__));
+    //Main plugin js file
+    wp_enqueue_script('iddt-admin.css', plugins_url('js/admin-voter.js', __FILE__), array('jquery'), '02021017', true);
+  }
+}
+
+add_action('admin_enqueue_scripts', 'iddt_voter_admin_enqueue_scripts');
 ?>
